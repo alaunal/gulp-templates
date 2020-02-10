1f832a9f1d7dc1669a824abc5ed9f89d10a16ce9
alaunal kauniyyah <a.kauniyyah@go-jek.com>
2020-02-10 19:12:38 +0700

    first commits

---
+++ README.md
@@ -0,0 +1,98 @@
+
+# GULP TEMPLATES
+
+**GULP TEMPLATES** is a tool that we designed to make it easier for developers (especially Front-end Developers) to create and create user interfaces as outlined in the visual web form of the design that has been determined.
+
+
+## Features
+
+This **WEB UIKIT** provides a simple way of setting up a modern web development environment. Here is a list of the current features:
+
+-  [**webpack-stream**](https://openbase.io/js/webpack-stream) Run webpack as a stream to conveniently integrate with gulp.
+-  [**GULP 4**](https://gulpjs.com/) Automate and enhance your workflow.
+-  [**Nunjucks**](https://mozilla.github.io/nunjucks/) A rich and powerful templating language for JavaScript.
+-  [**ES2015 Babel**](https://babeljs.io/) transpiler that allows you writing JS Code in ES2015/ES6 style.
+-  [**Sass**](http://sass-lang.com/): CSS pre-processor with [gulp-autoprefixer](https://www.npmjs.com/package/gulp-autoprefixer).
+-  [**Browsersync**](https://browsersync.io/) with Live reload.
+-  Minifies and optimize your javascript.
+
+
+## Requirements
+
+This should be installed on your computer in order to get up and running:
+
+-  [**Node.js**](https://nodejs.org/en/) Required node version is >= `10.0`
+-  **npm** Version `6.0.*`
+
+> If you've previously installed gulp globally, run `npm rm --global` gulp before following these instructions.
+
+
+
+## Usage
+
+As a prerequisite it's assumed you have `npm` or `yarn` installed.
+
+
+
+1.  **Clone Repo**
+
+	Make sure you have a **GULP STARTER-KIT** clone repository.
+	```
+	https://github.com/alaunal/gulp-templates.git
+	```
+	after you clone you will get structure directory like this :
+
+
+	```
+		[gulp-templates]
+			├── src/
+			├── .gitignore
+			├── gulpfile.js
+			├── gulp.config.js
+			├── site.config.js
+			├── .editorconfig
+			├── .jshintrc
+			├── .jsbeautifyrc
+			├── package.json
+			├── README.md
+	```
+
+2.  **Gulp Setup**
+
+	you just execute this script `npm install --global gulp-cli`, and make sure your Gulp CLI is currently in the version `2.0.*`
+
+3.  **Install dependencies**
+
+	```
+	$ npm install
+	```
+
+	> if you have done the syntax above before, there is no need to do a step 3 process. but if you are not sure then just do it for check updated.
+
+4.  **Serve or deploy**
+
+	When we start the `serve` process, the task runner below has `env` **development** and automatically `watch` the changes you make to the code.
+
+	```
+	$ yarn serve
+	```
+
+	or
+
+	```
+	$ npm run serve
+	```
+
+
+	> We have two environment build tasks in the development process or for deployment production.
+
+
+
+	**Development Build**
+
+	- development watch --> `yarn watch` or `npm run watch`
+	- development compile --> `yarn dev` or `npm run dev`
+
+	**Production Build**
+
+	- Production compile --> `yarn build` or `npm run build`
---
+++ gulpfile.js
@@ -0,0 +1,249 @@
+/*
+ *
+ * A simple Gulp 4 Starter Kit templates
+ *
+ * @author A.kauniyyah <a.kauniyyah@go-jek.com>
+ * @copyright 2019 A.kauniyyah | Sr. Front-end Web developer
+ *
+ * ________________________________________________________________________________
+ *
+ * gulpfile.js
+ *
+ * The gulp task runner file.
+ *
+ */
+
+// -- General 
+const gulp = require('gulp');
+const del = require('del');
+const header = require('gulp-header');
+const sourcemaps = require('gulp-sourcemaps');
+const noop = require('gulp-noop');
+const cache = require('gulp-cached');
+const pump = require('pump');
+const browserSync = require('browser-sync').create();
+const runSequence = require('gulp4-run-sequence');
+const plumber = require('gulp-plumber');
+
+// -- config
+const package = require('./package.json');
+const config = require('./gulpfile.config');
+const siteConf = require('./site.config');
+
+// -- Styles
+const sass = require('gulp-sass');
+const cleanCss = require('gulp-clean-css');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('gulp-autoprefixer');
+const cssnano = require('cssnano');
+const csso = require('gulp-csso');
+
+// -- HTML templates nunjuncks
+const nunjucksRender = require('gulp-nunjucks-render');
+const beautify = require('gulp-jsbeautifier');
+const data = require('gulp-data');
+
+// -- scripts
+const terser = require('gulp-terser');
+const optimizejs = require('gulp-optimize-js');
+const webpackStream = require('webpack-stream');
+const webpack = require('webpack');
+const babel = require('gulp-babel');
+const strip = require('gulp-strip-comments');
+const named = require('vinyl-named');
+
+// ---------------------------------------------------
+// -- FUNCTION OF HELPERS
+// ---------------------------------------------------
+
+// -- Environment configuration.
+
+const isProd = process.env.NODE_ENV === 'production';
+
+// -- fetch command line arguments
+
+const arg = (argList => {
+    let arg = {},
+        a, opt, thisOpt, curOpt;
+    for (a = 0; a < argList.length; a++) {
+        thisOpt = argList[a].trim();
+        opt = thisOpt.replace(/^\-+/, '');
+        if (opt === thisOpt) {
+            if (curOpt) arg[curOpt] = opt;
+            curOpt = null;
+        } else {
+            curOpt = opt;
+            arg[curOpt] = true;
+        }
+    }
+    return arg;
+})(process.argv);
+
+
+// ---------------------------------------------------
+// -- GULP TASKS
+// ---------------------------------------------------
+
+// -- clean of build dir
+
+gulp.task('clean', () => del(['./build']));
+
+// -- clean of cache
+
+gulp.task('clear-cache', done => {
+    cache.caches = {};
+
+    done();
+});
+
+// -- Run Server and reload setup
+
+gulp.task('runServer', () => {
+    return browserSync.init({
+        server: {
+            baseDir: ['build']
+        },
+        port: arg.port ? Number(arg.port) : 8080,
+        open: true
+    });
+});
+
+gulp.task('reload', done => {
+    browserSync.reload();
+    done();
+});
+
+// -- Scss of styles task runner compile
+
+gulp.task('compile-styles', done => {
+
+    if (!config.settings.styles) return done();
+
+    pump([
+        gulp.src(config.paths.styles.input),
+        (isProd ? noop() : sourcemaps.init()),
+        sass({
+            outputStyle: 'compressed'
+        }).on('error', sass.logError),
+        autoprefixer(),
+        postcss([
+            cssnano({
+                discardComments: {
+                    removeAll: true
+                }
+            })
+        ]),
+        csso(),
+        cleanCss({
+            level: {
+                1: {
+                    specialComments: 0
+                }
+            }
+        }),
+        (isProd ? noop() : sourcemaps.write('./maps')),
+        header(config.header.main, {
+            package: package
+        }),
+        gulp.dest(config.paths.styles.output)
+    ]);
+
+    done();
+});
+
+// -- Script js use rollup
+
+gulp.task('compile-scripts', done => {
+    if (!config.settings.scripts) return done();
+
+    return gulp.src(config.paths.scripts.dir + '*.js')
+        .pipe(plumber())
+        .pipe(named())
+        .pipe(webpackStream({
+            mode: 'production'
+        }, webpack))
+        .pipe(isProd ? noop() : sourcemaps.init())
+        .pipe(babel())
+        .pipe(terser(isProd ? config.uglify.prod : config.uglify.dev))
+        .pipe(optimizejs())
+        .pipe(strip())
+        .pipe((isProd ? noop() : sourcemaps.write('./maps')))
+        .pipe(header(config.header.main, {
+            package: package
+        }))
+        .pipe(gulp.dest(config.paths.scripts.output));
+});
+
+// -- Copy of static when of changed
+
+gulp.task('copy-static', () => {
+    return gulp.src(config.paths.libs + '**/*')
+        .pipe(gulp.dest(config.paths.output));
+});
+
+// -- Nunjucks html template compile 
+
+gulp.task('compile-html', done => {
+
+    if (!config.settings.copy) return done();
+
+    return gulp.src(config.paths.public.input)
+        .pipe(plumber())
+        .pipe(data(function() {
+            return siteConf.data;
+        }))
+        .pipe(nunjucksRender({
+            path: [config.paths.html]
+        }))
+        .pipe(beautify({
+            html: {
+                indent_size: 2,
+                indent_char: ' ',
+                max_preserve_newlines: 1
+            }
+        }))
+        .pipe(gulp.dest(config.paths.build));
+
+});
+
+// -- Compile task runner
+
+gulp.task('gulp:compile', function(callback) {
+    runSequence(
+        'clear-cache',
+        'compile-styles',
+        'compile-scripts',
+        'compile-html',
+        'copy-static',
+        callback
+    );
+});
+
+
+// -- watch task runner
+
+gulp.task('gulp:watch', () => {
+    gulp.watch(config.paths.src, callback => {
+        runSequence(
+            'gulp:compile',
+            'reload',
+            callback
+        );
+    });
+});
+
+// -- task serve
+
+gulp.task('gulp:serve', (callback) => {
+    runSequence(
+        'gulp:compile',
+        [
+            'runServer', 'gulp:watch'
+        ],
+        callback
+    );
+});
+
+// -- task default
+
+gulp.task('default', gulp.series('clean', 'gulp:compile'));
