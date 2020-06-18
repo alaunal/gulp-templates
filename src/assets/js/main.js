a14901e7991ef2e3e50ac35a8f9911ce3058ad92
alaunal kauniyyah <a.kauniyyah@go-jek.com>
2020-06-18 11:51:09 +0700

    create of signin page

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,6 +1,20 @@
-import axios from 'axios';
-
 window.addEventListener('load', () => {
 
+    const signIn = document.querySelectorAll('.c-signin');
+    if (signIn.length > 0) {
+        console.log('page sign in here!');
+        
+        const signUpButton = document.getElementById('signUp');
+        const signInButton = document.getElementById('signIn');
+        const wrapper = document.getElementById('wrapper');
+
+        signUpButton.addEventListener('click', () => {
+            wrapper.classList.add("active");
+        });
+
+        signInButton.addEventListener('click', () => {
+            wrapper.classList.remove("active");
+        });
+    }
 
 });
